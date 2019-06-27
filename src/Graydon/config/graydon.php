a3ee3784a06f2acb34ba8b313b408d9dda2d4dad
Shabbir Bharmal <shabbir@innotec.co.uk>
2019-06-27 15:04:27 +0530

    Changed config for graydon.

--- src/Graydon/config/graydon.php
+++ src/Graydon/config/graydon.php
@@ -1,23 +1,23 @@
 <?php
 return [
-    'ACCOUNT_ID' => '5cc8ad1a-2f92-447f-9c4c-a48e6560cf4f',
-    'CLIENT_ID' => 'cb575b5af51a44b9a26d8219361d0915',
-    'CLIENT_SECRET' => '71Fc2036c0D44B608812B9f36633d8A1',
-    'COUNTRY_CODE' => 'gb',
-    'ACCEPT' => 'application/json',
-    'SEARCH_TYPE' => 'GDM',
-    'IS_MOCK' => true,
-    'MONITORING_PROFILE_ID'=>'9aecfa18-21c9-4497-9bac-552d2d63e5a5',
+    'ACCOUNT_ID' => env('GRAYDON_ACCOUNT_ID'),
+    'CLIENT_ID' => env('GRAYDON_CLIENT_ID'),
+    'CLIENT_SECRET' => env('GRAYDON_CLIENT_SECRET'),
+    'COUNTRY_CODE' => env('GRAYDON_COUNTRY_CODE','gb'),
+    'ACCEPT' => env('GRAYDON_ACCEPT','application/json'),
+    'SEARCH_TYPE' => env('GRAYDON_SEARCH_TYPE','GDM'),
+    'IS_MOCK' => env('GRAYDON_IS_MOCK',false),
+    'MONITORING_PROFILE_ID' => env('GRAYDON_MONITORING_PROFILE_ID'),
 
-    'MOCK_SEARCH_END_POINT'=>'https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/graydon-search-api-v1.0.0/1.0.2/m/{country_id}/companies',
-    'SEARCH_END_POINT'=>'https://api.graydon.io/{country_id}/companies',
+    'MOCK_SEARCH_END_POINT' => env('GRAYDON_MOCK_SEARCH_END_POINT','https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/graydon-search-api-v1.0.0/1.0.2/m/{country_id}/companies'),
+    'SEARCH_END_POINT' => env('GRAYDON_SEARCH_END_POINT','https://api.graydon.io/{country_id}/companies'),
 
-    'MOCK_MONITORING_END_POINT'=>'https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/monitoring-api-v1.0.0/1.0.1/m/{country_id}/companies/monitoring/profiles/{profile_id}/events',
-    'MONITORING_END_POINT'=>'https://api.graydon.io/{country_id}/companies/monitoring/profiles/{profile_id}/events',
+    'MOCK_MONITORING_END_POINT' => env('GRAYDON_MOCK_MONITORING_END_POINT','https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/monitoring-api-v1.0.0/1.0.1/m/{country_id}/companies/monitoring/profiles/{profile_id}/events'),
+    'MONITORING_END_POINT' => env('GRAYDON_MONITORING_END_POINT','https://api.graydon.io/{country_id}/companies/monitoring/profiles/{profile_id}/events'),
 
-    'MOCK_MONITORING_SET_END_POINT'=>'https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/monitoring-api-v1.0.0/1.0.1/m/{country_id}/companies/monitoring/profiles/{profile_id}/events',
-    'MONITORING_SET_END_POINT'=>'https://api.graydon.io/{country_id}/companies/monitoring/profiles/{profile_id}/events',
+    'MOCK_MONITORING_SET_END_POINT' => env('GRAYDON_MOCK_MONITORING_SET_END_POINT','https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/monitoring-api-v1.0.0/1.0.1/m/{country_id}/companies/monitoring/profiles/{profile_id}/events'),
+    'MONITORING_SET_END_POINT' => env('GRAYDON_MONITORING_SET_END_POINT','https://api.graydon.io/{country_id}/companies/monitoring/profiles/{profile_id}/events'),
 
-    'MOCK_COMPANY_END_POINT'=>'https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/company-api-v1.0.0/1.0.9/m/{country_id}/companies/{company_id}/{other_uri}',
-    'COMPANY_END_POINT'=>'https://api.graydon.io/{country_id}/companies/{company_id}/{other_uri}',
+    'MOCK_COMPANY_END_POINT' => env('GRAYDON_MOCK_COMPANY_END_POINT','https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/{account_id}/company-api-v1.0.0/1.0.9/m/{country_id}/companies/{company_id}/{other_uri}'),
+    'COMPANY_END_POINT' => env('GRAYDON_COMPANY_END_POINT','https://api.graydon.io/{country_id}/companies/{company_id}/{other_uri}'),
 ];
